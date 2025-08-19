03a8050f6f8a418968fccac51d516a67c532e8d8
Kent <nikita5102007@gmail.com>
2025-08-19 10:55:57 -0700

    next lesson

--- firstcalculator.py
+++ firstcalculator.py
@@ -2,6 +2,6 @@
 print("Привет! Это калькулятор который только плюсует.")
 num1 = input("Введите первое число: ")
 num2 = input("Введите второе число: ")
-result = str(num1) + str(num2)
+result = float(num1) + float(num2)
 
 print("Ваш ответ:", result)
