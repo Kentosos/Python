2c0642349ffe0f1e670fe1037b0dbd785db4cdff
Kent <nikita5102007@gmail.com>
2025-08-22 14:38:23 -0700

    C.A.L.C

--- learn.py
+++ learn.py
@@ -1,2 +1,13 @@
 
-say_hsi()
+print('This is a C.A.L.C.U.L.A.T.O.R.')
+print('Computational Algorithm for Logical Calculations, Universal Linear Approximations, Tensor Operations & Results')
+print('|-----------------|')
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+print('|-----------------|')
+
+result = float(num1) + float(num2)
+
+print('Answer is', result)
